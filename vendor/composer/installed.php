da165c2d51e74d53834ca6d897c9ec8435bdc8a3
Damon Sharp <damon.sharp@gmail.com>
2024-09-28 11:28:11 -0400

    doh! we kinda need the vendor folder here

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'damonsharp/plugin-collections',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'fe7f9fddf6d55fe175909e0637e51c1250b7915a',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'damonsharp/plugin-collections' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'fe7f9fddf6d55fe175909e0637e51c1250b7915a',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
